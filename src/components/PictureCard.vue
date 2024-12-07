ecd60a114b4e2f7bf9c2563da77c16cb8edcc471
Ilya <ilya.nevolin.2004@mail.ru>
2024-12-07 22:31:37 +0500

    сделал адапив фильтров по странам

---
+++ src/components/PictureCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="picture__card">
+        <img :src="pictureData.url" :alt="pictureData.pictureName">
+        <p>{{ pictureData.author }}</p>
+        <h3>{{ pictureData.pictureName }}</h3>
+        <p>{{ pictureData.style }} {{ pictureData.size }}</p>
+        <p> {{ pictureData.price }}</p>
+        <button>В корзину</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pictureData: Object
+    }
+}
+</script>
--- src/components/ReproductionsGrid.vue
+++ src/components/ReproductionsGrid.vue
@@ -1,5 +1,155 @@
 <template>
-    <div>
-        
+    <div class="reproductions">
+        <p v-if="!reproductions">Загрузка...</p>
+        <div v-else>
+            <div class="reproductions__filters">
+                <h2 class="reproduction__header">Репродукции</h2>
+                <div class="reproduction__countries">
+                    <button class="reproduction__country button" v-for="(item, key) in reproductions" :key="key"
+                        @click="changeReproduction(item)">{{ item.Country }}</button>
+                </div>
+            </div>
+            <div class="reproductions__grid">
+                <PictureCard v-for="(picture, index) in pictures" :key="index" :picture-data="picture" />
+            </div>
+        </div>
     </div>
 </template>
+<script>
+import PictureCard from './PictureCard.vue';
+
+export default {
+    data() {
+        return {
+            reproductions: null,
+            pictures: null
+        }
+    },
+    methods: {
+        changeReproduction(item) {
+            this.pictures = item.Reproductions;
+            console.log(this.pictures);
+        }
+    },
+    mounted() {
+        fetch("reproductions.json")
+        .then(resp => resp.json())
+        .then(json => {
+            this.reproductions = json;
+        });
+    },
+    components: {
+        PictureCard
+    }
+}
+</script>
+
+<style>
+.reproductions__filters {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    max-width: 1110px;
+    margin: 0 auto;
+}
+
+.reproduction__header {
+    font-family: Raleway;
+    font-size: 40px;
+    font-weight: 500;
+    line-height: 44px;
+    color: #2C2D35;
+}
+
+.reproduction__countries {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    overflow-x: scroll;
+    gap: 0 32px;
+}
+
+.reproduction__countries::-webkit-scrollbar {
+    width: 0;
+}
+
+.reproduction__country.button {
+    border: none;
+    padding: 10px 20px;
+    background-color: #D4E8D9;
+    color: #2C2D35;
+    border-radius: 20px;
+    font-family: Raleway;
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 23.48px;
+}
+
+@media screen and (width <= 769px) {
+    .reproductions__filters {
+        max-width: 690px;
+        padding: 0 39px;
+    }
+
+    .reproduction__header {
+        font-size: 30px;
+        font-weight: 500;
+        line-height: 33px;
+    }
+
+    .reproduction__country.button {
+        font-size: 20px;
+        font-weight: 500;
+        line-height: 23.48px;
+    }
+}
+
+@media screen and (width <= 577px) {
+    .reproductions__filters {
+        max-width: 510px;
+        padding: 0 33px;
+    }
+
+    .reproduction__header {
+        font-size: 24px;
+        font-weight: 500;
+        line-height: 26.4px;
+        margin: 20px inherit;
+    }
+
+    .reproduction__country.button {
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 21.13px;
+    }
+}
+
+@media screen and (width <= 376px) {
+    .reproductions__filters {
+        max-width: 290px;
+        padding: 0 15px;
+    }
+
+    .reproduction__header {
+        font-size: 24px;
+        font-weight: 500;
+        line-height: 26.4px;
+    }
+
+    .reproduction__countries {
+        gap: 0 20px;
+    }
+
+    .reproduction__country.button {
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 21.13px;
+    }
+}
+
+@font-face {
+    font-family: Raleway;
+    src: url("/public/fonts/Raleway-VariableFont_wght.ttf");
+}
+</style>
